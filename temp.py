560c56f4489ffdc0110c442c0dce379e2338d67a
nikita-xxl <56357222+nikita-xxl@users.noreply.github.com>
2019-10-09 18:18:36 +0200

    lab#1

---
+++ temp.py
@@ -0,0 +1,37 @@
+print('------Задание№1------')
+print('Hello ,World!')
+print('Маша+Петя=Любовь')
+x=3+4
+print('x=',x)
+print()
+
+print('------Задание№2------')
+h='Hello, World!'
+print(type(h))
+a=3+4
+print(type(a))
+b=3/4
+print(type(b))
+x=[1,2,5,10,100]
+print(type(x))
+print()
+
+print('------Задание№3------')
+x=3
+y=(x**2)**0.5/(x**3+3/x)*(4*x**7-x**5)+80*(27*x**4+12*x**3-5*x**2+10)**0.5
+print(y)
+print()
+
+print('------Задание№4------')
+a=[1,5,'|Good','Bad']
+b=[9,'Blue','Red',11]
+x=a[1]+b[3]
+print(x)
+x=a[2]+b[2]
+print(x)
+x=a[0]*b[0]
+print(x)
+x=a[1]**b[3]
+print(x)
+x=a+b
+print(x)
